29964c7527b25cd0a6dd2759188630cfa57c706d
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-10-09 00:55:25 +0200

    [~] [Install/Remove]

--- Install.sh
+++ Install.sh
@@ -4,8 +4,9 @@
 arduino-cli \
   lib install \
     --git-url \
+      https://github.com/martin-eden/Embedded-me_BaseTypes \
       https://github.com/martin-eden/Embedded-me_MemorySegment \
-      https://github.com/martin-eden/Embedded-me_BaseTypes
+      https://github.com/martin-eden/Embedded-me_Console
 
 # Install library
 arduino-cli \
--- Remove.sh
+++ Remove.sh
@@ -5,5 +5,6 @@ arduino-cli lib uninstall me_ManagedMemory
 
 # Uninstall dependencies
 arduino-cli lib uninstall \
+  me_Console \
   me_MemorySegment \
   me_BaseTypes
